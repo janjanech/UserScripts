fe3be3bba664a800d6ea6cd102d66a4f2861c775
Jan Janech <jan.janech@kst.uniza.sk>
2022-02-11 18:20:58 +0100

    import-znamok vyuziva kniznicu funkcii pre system vzdelavanie

--- vzdelavanie.uniza.sk/import-znamok.user.js
+++ vzdelavanie.uniza.sk/import-znamok.user.js
@@ -1,123 +1,110 @@
 // ==UserScript==
 // @name         Import znamok z JSON na vzdelavanie
 // @namespace    https://vzdelavanie.uniza.sk/
-// @version      0.1
+// @version      0.2
 // @description  skript na pridanie moznosti importu znamok z JSON do systemu vzdeavanie
 // @author       Jan Janech
 // @match        https://vzdelavanie.uniza.sk/vzdelavanie/znpredmet.php?*
 // @icon         https://vzdelavanie.uniza.sk/favicon.ico
 // @grant        none
+// @require      https://raw.githubusercontent.com/janjanech/UserScripts/master/vzdelavanie.uniza.sk/_lib.js
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    let control_table = document.querySelector('#id-tabulka-znamky > tbody > tr > td > table')
-    let new_row = control_table.insertRow(2)
-    let control_cell = new_row.insertCell(0)
+    jj_ext.create_toolbutton("Import znamok z jsonu", function() {
+        jj_ext.create_dialog(
+            function(dialog) {
+                let znamky_json_label = document.createElement('label');
+                znamky_json_label.setAttribute("for", 'znamky_json');
+                znamky_json_label.innerHTML = '<b>JSON znamky:</b>';
 
-    control_cell.setAttribute("align", "center");
-    control_cell.setAttribute("colspan", "4");
+                let znamky_json = document.createElement('textarea');
+                znamky_json.setAttribute("id", 'znamky_json');
+                znamky_json.setAttribute("rows", '25');
+                znamky_json.setAttribute("style", 'width: 100%');
+                znamky_json.setAttribute("class", 'tform');
 
-    let show_button = document.createElement('input')
-    show_button.setAttribute("type", 'button')
-    show_button.setAttribute("class", 'tien')
-    show_button.setAttribute("value", 'Import znamok z jsonu')
-    show_button.addEventListener('click', function() {
-        show_button.disabled = true;
+                let datum_text_label = document.createElement('label');
+                datum_text_label.setAttribute("for", 'datum_text');
+                datum_text_label.innerHTML = '<b>Datum skusky:</b>';
 
-        let znamky_json_label = document.createElement('label');
-        znamky_json_label.setAttribute("for", 'znamky_json');
-        znamky_json_label.innerHTML = '<b>JSON znamky:</b>';
+                let today = new Date();
+                let datum_text = document.createElement('input');
+                datum_text.setAttribute("class", 'tform');
+                datum_text.value = today.getDate() + '.'+ (today.getMonth()+1) + '.' + today.getFullYear();
 
-        let znamky_json = document.createElement('textarea');
-        znamky_json.setAttribute("id", 'znamky_json');
-        znamky_json.setAttribute("rows", '25');
-        znamky_json.setAttribute("style", 'width: 100%');
-        znamky_json.setAttribute("class", 'tform');
+                dialog.appendChild(document.createElement('br'));
+                dialog.appendChild(znamky_json_label);
+                dialog.appendChild(document.createElement('br'));
+                dialog.appendChild(znamky_json);
+                dialog.appendChild(document.createElement('br'));
+                dialog.appendChild(datum_text_label);
+                dialog.appendChild(document.createElement('br'));
+                dialog.appendChild(datum_text);
 
-        let datum_text_label = document.createElement('label');
-        datum_text_label.setAttribute("for", 'datum_text');
-        datum_text_label.innerHTML = '<b>Datum skusky:</b>';
+                return {
+                    'znamky': znamky_json,
+                    'datum': datum_text
+                };
+            }, function(state) {
+                let znamky = JSON.parse(state.znamky.value);
 
-        let today = new Date();
-        let datum_text = document.createElement('input');
-        datum_text.setAttribute("class", 'tform');
-        datum_text.value = today.getDate() + '.'+ (today.getMonth()+1) + '.' + today.getFullYear();
+                let datum = state.datum.value;
 
-        let import_button = document.createElement('input');
-        import_button.setAttribute("type", 'button');
-        import_button.setAttribute("class", 'tien');
-        import_button.setAttribute("value", 'Import');
+                let znamky_map = {};
 
-        control_cell.appendChild(document.createElement('br'));
-        control_cell.appendChild(znamky_json_label);
-        control_cell.appendChild(document.createElement('br'));
-        control_cell.appendChild(znamky_json);
-        control_cell.appendChild(document.createElement('br'));
-        control_cell.appendChild(datum_text_label);
-        control_cell.appendChild(document.createElement('br'));
-        control_cell.appendChild(datum_text);
-        control_cell.appendChild(document.createElement('br'));
-        control_cell.appendChild(import_button);
+                let pocet_importovanych = 0;
 
-        import_button.addEventListener('click', function() {
-            let znamky = JSON.parse(znamky_json.value);
-
-            let datum = datum_text.value;
-
-            let znamky_map = {};
-
-            let pocet_importovanych = 0;
-
-            for (let item of znamky) {
-                let personal_no = item[0];
-                if (znamky_map.hasOwnProperty(personal_no)) {
-                    console.log(`Znamka pre ${personal_no} zadana 2x`);
+                for (let item of znamky) {
+                    let personal_no = item[0];
+                    if (znamky_map.hasOwnProperty(personal_no)) {
+                        console.log(`Znamka pre ${personal_no} zadana 2x`);
+                    }
+                    znamky_map[personal_no] = item[1];
                 }
-                znamky_map[personal_no] = item[1];
-            }
 
-            for (let row of document.querySelectorAll('#znamkovanie tr:not(.hdr)')) {
-                let personal_no = row.cells[2].innerText;
-                let name = row.cells[1].innerText;
-                if (znamky_map.hasOwnProperty(personal_no)) {
-                    let znamka = znamky_map[personal_no].toUpperCase();
-                    delete znamky_map[personal_no]
+                for (let row of document.querySelectorAll('#znamkovanie tr:not(.hdr)')) {
+                    let personal_no = row.cells[2].innerText;
+                    let name = row.cells[1].innerText;
+                    if (znamky_map.hasOwnProperty(personal_no)) {
+                        let znamka = znamky_map[personal_no].toUpperCase();
+                        delete znamky_map[personal_no]
 
-                    let found_col = -1;
-                    for (let col = 5; col <= 7; col++) {
-                        let old_znamka = row.cells[col].querySelector('select').value;
-                        if (!old_znamka || old_znamka === '0') {
-                            found_col = col;
-                            break;
-                        } else if (old_znamka !== 'FX') {
-                            break;
+                        let found_col = -1;
+                        for (let col = 5; col <= 7; col++) {
+                            let old_znamka = row.cells[col].querySelector('select').value;
+                            if (!old_znamka || old_znamka === '0') {
+                                found_col = col;
+                                break;
+                            } else if (old_znamka !== 'FX') {
+                                break;
+                            }
+                        }
+
+                        if (found_col === -1) {
+                            console.log(`Nemozem zmenit znamku pre ${name}`);
+                        } else {
+                            let znamka_select = row.cells[found_col].querySelector('select');
+                            let datum_input = row.cells[found_col].querySelector('input');
+
+                            datum_input.value = datum;
+                            znamka_select.value = znamka;
+
+                            pocet_importovanych++;
                         }
                     }
+                }
 
-                    if (found_col === -1) {
-                        console.log(`Nemozem zmenit znamku pre ${name}`);
-                    } else {
-                        let znamka_select = row.cells[found_col].querySelector('select');
-                        let datum_input = row.cells[found_col].querySelector('input');
-
-                        datum_input.value = datum;
-                        znamka_select.value = znamka;
-
-                        pocet_importovanych++;
+                for (let personal_no in znamky_map) {
+                    if (znamky_map.hasOwnProperty(personal_no)) {
+                        console.log(`Nepodarilo sa najst studenta s osobnym cislom ${personal_no}`);
                     }
                 }
-            }
 
-            for (let personal_no in znamky_map) {
-                if (znamky_map.hasOwnProperty(personal_no)) {
-                    console.log(`Nepodarilo sa najst studenta s osobnym cislom ${personal_no}`);
-                }
+                alert(`Importovanych ${pocet_importovanych} znamok`);
             }
-
-            alert(`Importovanych ${pocet_importovanych} znamok`);
-        });
+        );
     });
-    control_cell.appendChild(show_button)
 })();
